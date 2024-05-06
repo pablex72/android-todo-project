fee39d3aae1c73cf0491854bd8d489399ce397ba
pablo <tonyp2hoy@gmail.com>
2024-05-06 15:59:58 -0400

    feat: todo android app

---
+++ app/src/main/java/com/pablodev/todoapplication/todoapp/TodoActivity.kt
@@ -0,0 +1,17 @@
+package com.pablodev.todoapplication.todoapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.pablodev.todoapplication.R
+
+class TodoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_todo)
+
+    }
+}
